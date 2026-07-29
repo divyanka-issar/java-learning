246de31b35893c33652c49ce9ef604be8c858e1c
Divyanka Issar <divyanka.issar@gmail.com>
2026-07-29 20:28:56 +0530

    Added variables basics in java

---
+++ src/basics/variables.java
@@ -0,0 +1,34 @@
+package basics;
+
+/**
+ * Topic: Variables
+ * Description: Understanding declaration, initialization
+ * and usage of variables in java.
+ */
+
+public class variables {
+    public static void main(String[] args){
+
+        // Integer variable
+        int age=20;
+
+        // Decimal variable
+        double cgpa=9.10;
+
+        // Character variable
+        char grade='A';
+
+        // Boolean variable
+        boolean isStudent=true;
+
+        // String variable
+        String name="Divyanka";
+
+        // Printing variables
+        System.out.println("Age:" + age);
+        System.out.println("CGPA:" + cgpa);
+        System.out.println("Grade:" + grade);
+        System.out.println("Is Student:" + isStudent);
+        System.out.println("Name:" + name);
+    }
+}
