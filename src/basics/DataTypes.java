6464e81f98a2dcfa91298ad39e302b27571f006d
Divyanka Issar <divyanka.issar@gmail.com>
2026-07-31 16:46:50 +0530

    Add Java primitive data types examples

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,42 @@
+package basics;
+
+public class DataTypes{
+    public static void main(String[] args){
+
+        // Primitive Data Types
+
+        // byte (1 byte)
+        byte age = 20;
+
+        // short (2 bytes)
+        short year = 2026;
+
+        // int (4 bytes)
+        int salary = 50000;
+
+        // long (8 bytes)
+        long population = 1_000_000_000;
+
+        // float (4 bytes)
+        float temperature = 36.5f;
+
+        // double (8 bytes)
+        double pi = 3.141592653589793;
+
+        // char (2 bytes)
+        char grade = 'A';
+
+        // boolean (1 bit logically)
+        boolean isJavaFun = true;
+
+        // printing values
+        System.out.println("Age:" + age);
+        System.out.println("Year:" + year);
+        System.out.println("Salary:" + salary);
+        System.out.println("Population:" + population);
+        System.out.println("Temperature:" + temperature);
+        System.out.println("Pi:" + pi);
+        System.out.println("Grade:" + grade);
+        System.out.println("Is Java fun:" + isJavaFun);
+    }
+}
