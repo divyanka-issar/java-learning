7a642e506e6d82f9b33582920886e2e1f6ff3e12
Divyanka Issar <divyanka.issar@gmail.com>
2026-08-03 16:14:37 +0530

    Add arithmetic operators examples

---
+++ src/basics/Operators.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Operators {
+    public static void main(String[] args){
+
+        // Arithmetic Operators
+        int num1 = 20;
+        int num2 = 6;
+        System.out.println("Arithmetic Operators");
+
+        System.out.println("Addition:" + (num1 + num2));
+        System.out.println("Subtraction:" + (num1 - num2));
+        System.out.println("Multiplication:" + (num1 * num2));
+        System.out.println("Division:" + (num1 / num2));
+        System.out.println("Modulus:" + (num1 % num2));
+    }
+}
