3a26cf15baed48373a0da32a44982a2259a89d2c
Divyanka Issar <divyanka.issar@gmail.com>
2026-08-13 11:00:33 +0530

    Add else-if ladder, nested-if and switch statements

--- src/basics/ConditionalStatements.java
+++ src/basics/ConditionalStatements.java
@@ -25,6 +25,54 @@ public class ConditionalStatements {
         else {
             System.out.println("The number is odd");
         }
+
+        // else-if ladder statement
+        System.out.println("Enter marks :");
+        int marks = input.nextInt();
+        if(marks >=90){
+            System.out.println("Grade A");
+        }
+        else if(marks >=75){
+            System.out.println("Grade B");
+        }
+        else if(marks >=50){
+            System.out.println("Grade C");
+        }
+        else{
+            System.out.println("Fail");
+        }
+
+        // nested-if statement
+        System.out.println("Enter your standard :");
+        int standard = input.nextInt();
+        if(standard>=9){
+            if(standard>=11){
+                System.out.println("Senior high school");
+            }
+            else{
+                System.out.println("Junior high school");
+            }
+        }
+        else{
+            System.out.println("You are a junior");
+        }
+
+        // switch statement
+        System.out.println("Enter a number (1-3) :");
+        int choice = input.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("You selected Java");
+                break;
+            case 2:
+                System.out.println("You selected C++");
+                break;
+            case 3:
+                System.out.println("You selected Python");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
         input.close();
     }
 }
